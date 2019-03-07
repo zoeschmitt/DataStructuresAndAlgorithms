9399e1f74b424f28cd1758ddfe3f810c075542ec
Zoe Schmitt <mica@Zoes-MacBook-Pro.local>
2019-03-06 22:39:07 -0600

    update

--- stack/myStack.cpp
+++ stack/myStack.cpp
@@ -31,17 +31,6 @@ myStack::~myStack() {
 	 delete [] contents;
 }
 
-/*
- * Functions: push, pop
- * Usage: s1.push(element); element = s1.pop();
- * --------------------------------------------
- * These are the fundamental stack operations that add an element to
- * the top of the stack and remove an element from the top of the stack.
- * A call to pop on an empty stack or to push on a full stack
- * is an error.  Make use of isEmpty()/isFull() (see below)
- * to avoid these errors.
- */
-
 void myStack::push(int element) {
     
      if (isFull())
